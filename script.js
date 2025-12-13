682cd815b374211b91a82bc52e24bc4ccfdeaaed
Lars Geirnaert <lars.geirnaert@student.hogent.be>
2025-12-13 23:10:55 +0100

    Multiplayer update compleet

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ const firebaseConfig = {
   projectId: "weather-duel-cf13e",
   storageBucket: "weather-duel-cf13e.firebasestorage.app",
   messagingSenderId: "1066877072806",
-  appId: "1:1066877072806:web:068b650d5a5b62872c4b67"
+  appId: "1:1066877072806:web:068b650d5a5b62872c4b67",
+  databaseURL: "https://weather-duel-cf13e-default-rtdb.europe-west1.firebasedatabase.app"
 };
 // !!! ⚠️ EINDE CONFIG BLOK !!!
 
